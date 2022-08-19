f6298d93a4eae26d6240a95eb8a323ae60b7ca00
Sunaina <sunaina.sufi@gmail.com>
2022-08-19 16:23:53 +0530

    Pushing changes

--- my-project/src/app/bar/bar.component.ts
+++ my-project/src/app/bar/bar.component.ts
@@ -3,14 +3,14 @@ import { Form, NgForm } from '@angular/forms';
 import { mixinDisabled } from '@angular/material/core';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import data from '../../assets/json/data.json'
-interface questionAns {  
+interface questionAns {
   questionIndex: String,
   question: String,
-  ans1: string,  
-  ans2: String,  
-  ans3: String,  
+  ans1: string,
+  ans2: String,
+  ans3: String,
   ans4: String,
-} 
+}
 
 @Component({
   selector: 'app-bar',
@@ -18,70 +18,68 @@ interface questionAns {
   styleUrls: ['./bar.component.css']
 })
 export class BarComponent implements OnInit {
-progress:any=0;
-totalindex=0;
-checkprogressBar=0;
-answer: questionAns = data[0];  
-allQuestions: questionAns[] = data;
-progressBarIncrement: number = 0;
-indexQuestion=0;
-constructor() { }
-ngOnInit(): void {
-    // console.log(this.answer)
-   //  this.progressBarIncrement=100/this.allQuestions.length+1
-    for(let count=1;count<=this.allQuestions.length;count++)
-     {
-     this.totalindex++;
-     } 
-  }
+    progress:any=0;
+    totalindex=0;
+    checkprogressBar=0;
+    answer: questionAns = data[0];
+    allQuestions: questionAns[] = data;
+    progressBarIncrement: number = 0;
+    indexQuestion=0;
+    backHidden=true;
     isSubmitted = false;
+    constructor() { }
+
+      ngOnInit(): void {
+          for (let count = 1; count <= this.allQuestions.length; count++) {
+                this.totalindex++;
+          }
+          var element1 = <HTMLInputElement>document.getElementById("back");
+           if (this.indexQuestion == 0) {
+              element1.style.display = "none";
+           } else {
+             element1.style.display = "";
+           }
+      }
+
     submitForm(form: NgForm) {
         this.isSubmitted = true;
-         if(!form.valid) {
-        return false;
-      } 
-       else {
-              this.progressBarIncrement=100/this.totalindex+2;
-              this.indexQuestion =  Number(this.answer.questionIndex)
-               this.answer = data[this.indexQuestion+1]
-              // if(indexQuestion==this.allQuestions.length-1){
-              //     var element = <HTMLInputElement> document.getElementById("button");
-              //     element.disabled=true;
-              // }
-             
+        if (!form.valid) {
+          return false;
+        } else {
+          this.progressBarIncrement = 100 / this.totalindex;
+          this.indexQuestion = Number(this.answer.questionIndex)
+          if(this.indexQuestion< this.allQuestions.length)
+          this.answer = data[this.indexQuestion]
         }
-        //const indexQuestion =  Number(this.answer.questionIndex)
-        var element = <HTMLInputElement> document.getElementById("button");
-        if(this.indexQuestion==this.allQuestions.length-1){
-          
-          element.disabled=true;
-         }else{
-          element.disabled=false;
-         }
-         
-     return this.progress=this.progress+this.progressBarIncrement
-    
+        var element = <HTMLInputElement> document.getElementById("next");
+        var element1 = <HTMLInputElement>document.getElementById("back");
+        if (this.indexQuestion == this.allQuestions.length ) {
+          element.style.display = "none";
+        } else {
+          element.style.display = "";
+          element1.style.display = "";
+        }
+        return this.progress = this.progress + this.progressBarIncrement
+
     }
-   
-    progressbarBackword(){
-      this.indexQuestion =  Number(this.answer.questionIndex)
-      this.answer = data[this.indexQuestion-1]
-      this.progress=this.progress-this.progressBarIncrement
-    //     if(this.progress==0){
-    //     this.progress=this.progress+0
-    //  }
-    //  else { 
-      
-      
-      
-    //  }
-     var element = <HTMLInputElement> document.getElementById("button");
-     if(this.indexQuestion==this.allQuestions.length){
-      element.disabled=true;
-     }else{
-      element.disabled=false;
-     }
-     //this.answer = data[this.indexQuestion-1]
-  }
+
+    progressbarBackword() {
+      this.indexQuestion = Number(this.answer.questionIndex)
+      this.answer = data[this.indexQuestion - 2]
+      if (this.indexQuestion == this.totalindex) {
+        this.progress = this.progress - (this.progressBarIncrement * 2)
+      } else {
+        this.progress = this.progress - this.progressBarIncrement;
+      }
+
+      var element = <HTMLInputElement>document.getElementById("next");
+      if (Number(this.answer.questionIndex) == 1) {
+        var element1 = <HTMLInputElement>document.getElementById("back");
+        element1.style.display = "none";
+        //element.disabled = true;
+      } else {
+        element.style.display = "";
+      }
+    }
 }
-  
+
